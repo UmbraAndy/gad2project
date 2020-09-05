8c0505854283af2b1d403a85c16a4c97624bc280
Umbra <adeleke.a.babatunde@gmail.com>
2020-09-05 12:29:01 +0100

    WIP: Tab content

--- app/src/main/java/com/example/gads2/LearnerFragment.kt
+++ app/src/main/java/com/example/gads2/LearnerFragment.kt
@@ -1,10 +1,6 @@
 package com.example.gads2
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 
 class LearnerFragment : Fragment(R.layout.fragment_learner) {
