155f0824a31fbd7ed50d17235d9e3a8c8f1d47ea
Umbra <adeleke.a.babatunde@gmail.com>
2020-09-08 17:14:09 +0100

    WIP: screen restructure

---
+++ app/src/main/java/com/example/gads2/SubmissionFragment.kt
@@ -0,0 +1,13 @@
+package com.example.gads2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SubmissionFragment : Fragment(R.layout.fragment_submission) {
+
+
+}
