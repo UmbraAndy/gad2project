f5e9e88ecc4ad43d323958a2640c0213b5d04137
Umbra <adeleke.a.babatunde@gmail.com>
2020-09-08 18:02:33 +0100

    WIP: link tab to view pager

--- app/src/main/java/com/example/gads2/LeaderBoardMainFragment.kt
+++ app/src/main/java/com/example/gads2/LeaderBoardMainFragment.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_leader_board_main.*
 
 
@@ -17,16 +19,26 @@ class LeaderBoardMainFragment : Fragment(R.layout.fragment_leader_board_main) {
         //bind tab to pager
         activity?.let {
             mainViewPager.adapter = MainLeaderBoardPageAdapter(it)
+            TabLayoutMediator(tabLayout, mainViewPager) { tab, position ->
+                when (position) {
+                    0 -> tab.text = "Learning Leaders"
+                    1 -> tab.text = "Skill IQ Leaders"
+                }
+            }.attach()
         }
 
+        submitButton.setOnClickListener {
+            findNavController().navigate(R.id.action_leaderBoardMainFragment_to_submissionFragment)
+        }
     }
 
-    private class MainLeaderBoardPageAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity){
+    private class MainLeaderBoardPageAdapter(fragmentActivity: FragmentActivity) :
+        FragmentStateAdapter(fragmentActivity) {
         override fun getItemCount(): Int = 2
 
         override fun createFragment(position: Int): Fragment {
             Log.d("XXXX", "POS: $position")
-            return when(position){
+            return when (position) {
                 0 -> LearnerFragment()
                 1 -> SkillFragment()
                 else -> {
