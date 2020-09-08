8bd47ef3f2590ef03d038a86890714ec9e3e1818
Umbra <adeleke.a.babatunde@gmail.com>
2020-09-09 00:14:48 +0100

    WIP: Leaderboard styling complete

--- app/src/main/java/com/example/gads2/SubmissionFragment.kt
+++ app/src/main/java/com/example/gads2/SubmissionFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_submission.*
 
 
 class SubmissionFragment : Fragment(R.layout.fragment_submission) {
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        submitScreenBackButtton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
